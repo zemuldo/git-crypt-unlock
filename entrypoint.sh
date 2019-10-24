a3276612d52cf075ea3e399f5739661c19b6994a
Danstan Onyango <danstan.otieno@gmail.com>
2019-10-24 11:10:53 +0300

    minor

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,6 @@ gpgconf --kill gpg-agent
 
 gpg-agent --daemon --allow-preset-passphrase --max-cache-ttl 3153600000
 
-/usr/libexec/gpg-preset-passphrase --preset --passphrase $GPG_KEY_PASS "$GPG_KEY_GRIP"
+/usr/libexec/gpg-preset-passphrase --preset --passphrase "$GPG_KEY_PASS" "$GPG_KEY_GRIP"
 
 git-crypt unlock
