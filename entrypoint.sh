9b7a4f2a3253cd4cf5db10328eb63fadd9670ff2
Danstan Onyango <danstan.otieno@gmail.com>
2019-10-24 10:55:24 +0300

    fix import key

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Put GCP service account key from base64 to json on a file.
 echo "$GPG_PRIVATE_KEY" | base64 -d > "$HOME"/git-crypt-key.asc
 
-gpg --batch --import git-crypt-key.asc
+gpg --batch --import "$HOME"git-crypt-key.asc
 
 gpgconf --kill gpg-agent
 
