480e669d010e23c1eae33da72c779cf1f5992fb8
Danstan Onyango <danstan.otieno@gmail.com>
2019-10-24 09:54:42 +0300

    dummy stages

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,5 @@
 
 # Put GCP service account key from base64 to json on a file.
 sh -c "ls"
-sh -c "git-crypt unlock"
-sh -c "gpg --batch --import git-crypt-key.asc"
+sh -c "echo hello-world > catme.txt"
 sh -c "ls"
