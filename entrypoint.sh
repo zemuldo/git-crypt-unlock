3b33f462893db691cc248b174871b36eb7d162e5
Danstan Onyango <danstan.otieno@gmail.com>
2019-10-24 10:21:28 +0300

    adds git crypt stage

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 # Put GCP service account key from base64 to json on a file.
-ls
-echo "Trying cat"
-sudo echo "hello-world" > catme.txt
-ls
+echo "$GPG_PRIVATE_KEY" | base64 -d > "$HOME"/git-crypt-key.asc
+
+gpg --batch --import git-crypt-key.asc
+
+gpgconf --kill gpg-agent
+
+gpg-agent --daemon --allow-preset-passphrase --max-cache-ttl 3153600000
+
+/usr/lib/gnupg2/gpg-preset-passphrase --preset --passphrase $GPG_KEY_PASS "$GPG_KEY_GRIP"
+
+git-crypt unlock
