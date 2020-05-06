a9731e4ada2fffccb5d1efa0d8c7f8fd02ca4fb0
zemuldo <danstan.otieno@gmail.com>
2020-05-07 02:01:09 +0300

    simplify

--- src/installer.js
+++ src/installer.js
@@ -3,37 +3,13 @@ const path = require('path')
 
 module.exports = async function (platform) {
     if(platform == "darwin"){
-        console.log("Setting up on OS X")
-        await installGnuPGDarwin()
-        await installGitCryptDarwin()
+        console.log("Setting up on OS X \n")
+        await exec(path.join(__dirname, 'darwin.sh'))
     }
 
     else if(platform == "linux") {
         console.log("Setting up on Linux")
-        await installGnuPGLinux()
-        await installGitCryptLinux()
+        await exec(path.join(__dirname, 'linux.sh'))
     }
 
-}
-
-/**
- * Install GnuPG.
- *
- */
-async function installGnuPGLinux() {
-    await exec(path.join(__dirname, 'linux/install-gpg.sh'))
-}
-async function installGnuPGDarwin() {
-    await exec(path.join(__dirname, 'darwin/install-gpg.sh'))
-}
-
-/**
- * Install Git-Crypt.
- *
- */
-async function installGitCryptLinux() {
-    await exec(path.join(__dirname, 'linux/install-git-crypt.sh'))
-}
-async function installGitCryptDarwin() {
-    await exec(path.join(__dirname, 'darwin/install-git-crypt.sh'))
 }
