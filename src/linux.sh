37b61c8c9b0885564792b6156cbe060fcb55c5cd
zemuldo <danstan.otieno@gmail.com>
2020-05-07 02:23:27 +0300

    untrack nodemodules

--- src/linux.sh
+++ src/linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+echo "Setting up GnbuPG"
 
-# Put GCP service account key from base64 to json on a file.
 echo "$GPG_PRIVATE_KEY" | base64 -d > "$HOME"/git-crypt-key.asc
 
 gpg --batch --import "$HOME"/git-crypt-key.asc
@@ -11,6 +11,7 @@ gpg-agent --daemon --allow-preset-passphrase --max-cache-ttl 3153600000
 
 /usr/lib/gnupg2/gpg-preset-passphrase --preset --passphrase $GPG_KEY_PASS "$GPG_KEY_GRIP"
 
+echo "Intalling GitCrypt"
 sudo apt-get install git-crypt
 
 git-crypt unlock
