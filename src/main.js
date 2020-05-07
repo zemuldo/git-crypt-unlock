31302c524ff9e4f6a701ec4ed43774cceb138c8a
zemuldo <danstan.otieno@gmail.com>
2020-05-07 23:10:52 +0300

    package

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
 const core = require('@actions/core')
 const {exec} = require('@actions/exec')
-const installer = require('./installer')
 const path = require('path')
-const semver = require('semver')
-const https = require('https')
 
 main().catch(err => {
   core.setFailed(err.message)
 })
 
 async function main() {
-    if(process.platform == "darwin"){
-        console.log("Setting up on OS X \n")
-        await exec(path.join(__dirname, 'darwin.sh'))
-    }
-
-    else if(process.platform == "linux") {
-        console.log("Setting up on Linux")
-        await exec(path.join(__dirname, 'linux.sh'))
+    try {
+        if(process.platform == "darwin"){
+            console.log("Setting up on OS X \n")
+            await exec(path.join(__dirname, 'darwin.sh'))
+        }
+    
+        else if(process.platform == "linux") {
+            console.log("Setting up on Linux")
+            await exec(path.join(__dirname, 'linux.sh'))
+        }
+    } catch(error){
+        console.error(error)
+        process.exit(1)
     }
 }
