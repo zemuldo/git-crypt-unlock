211e351e3194371129355e4a156cd88706d61dc0
zemuldo <danstan.otieno@gmail.com>
2020-05-08 18:25:06 +0300

    clean scripts

--- dist/run/darwin.sh
+++ dist/run/darwin.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-echo "Exporting key from base64 to file "$HOME"/git-crypt-key.asc"
+if [ -n "$GPG_PRIVATE_KEY" ]
+ then
+  echo "Setting up GnbuPG"
+else
+  echo "ERROR: This action can not run without Base 64 encoded GPG key in Variable GPG_PRIVATE_KEY"
+  exit 1
+fi
+
 echo "$GPG_PRIVATE_KEY" | base64 -D > "$HOME"/git-crypt-key.asc
 
-echo "Importing GPG Private Key"
 gpg --batch --import "$HOME"/git-crypt-key.asc
 
-echo "Trusting all keys"
+rm "$HOME"/git-crypt-key.asc
+
 for fpr in $(gpg --list-keys --with-colons  | awk -F: '/fpr:/ {print $10}' | sort -u); do  echo -e "5\ny\n" |  gpg --command-fd 0 --expert --edit-key $fpr trust; done
 
 echo "Kill GPG agent and allow preset pass"
@@ -14,10 +21,18 @@ gpgconf --kill gpg-agent
 
 gpg-agent --daemon --allow-preset-passphrase --max-cache-ttl 3153600000
 
-echo "Cache passphrase in agent daemon"
 GPG_PATH=`gpgconf --list-dirs libexecdir`
-echo $GPG_PATH
 
-$GPG_PATH/gpg-preset-passphrase -c $GPG_KEY_GRIP <<<  $GPG_KEY_PASS
+if [ -n "$GPG_KEY_PASS" ] && [ -n "$GPG_KEY_GRIP" ]
+ then
+  gpgconf --kill gpg-agent
 
+  gpg-agent --daemon --allow-preset-passphrase --max-cache-ttl 3153600000
+
+  $GPG_PATH/gpg-preset-passphrase -c $GPG_KEY_GRIP <<<  $GPG_KEY_PASS
+else
+  echo echo -e "\033[33mGPG Key Grip and Passphrase not provided, Skipping and will run without\033[0m"
+fi
+
+echo "Intalling GitCrypt"
 HOMEBREW_NO_AUTO_UPDATE=1 brew install git-crypt
